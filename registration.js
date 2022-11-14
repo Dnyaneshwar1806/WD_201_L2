f1f9750a3a6d9ba169efb345587553a75d9b4133
Dnyaneshwar Kolape <113690251+Dnyaneshwar1806@users.noreply.github.com>
2022-11-15 01:21:57 +0530

    Update registration.js

--- registration.js
+++ registration.js
@@ -1,54 +1,73 @@
-<html>
-<head>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-<body style="text-align: center">  
-    <form id="user-form" method="post">
-    <div style="
-        border-radius: 50px;
-        border: 10px solid transparent;
-        margin-left: 450px;
-        margin-right: 450px;
-        background: linear-gradient(45deg,red,blue) border-box">
-      <h1 style="text-decoration: underline; color: beige;"/>Details of Candidate</h1>
+/* eslint-disable quotes */
+let userForm = document.getElementById("user-form");
+var userEntries = [];
 
-      <li ><label for="text">Name: </label>
-      <input required type="text" id="name" name="name"></li>
-      <p></p>
-        <li><label for="email">Email: </label>
-      <input required type="email" id="email" name="email" ></li>
-      <p></p>
-        <li><label required for="password">Password: </label>
-      <input required type="password" id="password" name="password" ></li>
-      <p></p>
-        <li><label for="dob">Date of Birth: </label>
-      <input type="date" id="dob" name="dob"></li>
-      <p></p>
-      <label for="acceptTerms">Accepted Terms & Conditions </label>
-      <input type="checkbox" name="acceptTerms" id="acceptTerms" >
-      <p></p>
-      <button required class="button" type="submit" style="--x: 50%;
-      --y: 50%;
-      position: relative;
-      appearance: none;
-      padding: 1em 2em;
-      color: white;
-      cursor: pointer;
-      outline: none;
-      border-radius: 100px;
-  
-      border: 2px solid transparent;
-      background: linear-gradient(#000, #000) padding-box, radial-gradient(farthest-corner at var(--x) var(--y), #00C9A7, #845EC2) border-box;
-  ">submit</button>
-      <p></p>
-    </div>
-  </form>
-  <div class="relative bg-white px-4 mt-5 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:rounded-lg sm:px-10 ">
-    <div class="mx-auto">
-        <h2 class="text-xl text-center font-bold leadin-right">Entries</h2>
-        <div class="divide-y divide-gray-300/50" id="user-entries"></div>
-    </div>
-</div>
-  <script src="registrationStyling.js"></script>
-</body>
-</html>
+const retieveEntries = () => {
+  let entries = localStorage.getItem("userEntries");
+  if (entries) {
+    entries = JSON.parse(entries);
+  } else {
+    entries = [];
+  }
+  return entries;
+};
+const displayEntries = () => {
+  let entries = retieveEntries();
+  const tbleEntries = entries
+    .map((entry) => {
+      const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+      const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+      const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+      const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+      const acceptTermsCell = `<td class='border px-4 py-2'>${entry.acceptTerms}</td>`;
+      const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${acceptTermsCell}</tr>`;
+      return row;
+    })
+    .join("\n");
+  const table = ` <table class='table-auto w-full'>
+    <tr>
+    <th class='px-4 py-2 '>Name </th>
+    <th class='px-4 py-2 '>Email </th>
+    <th class='px-4 py-2 '>Password </th>
+    <th class='px-4 py-2 '>Dob </th>
+    <th class='px-4 py-2 '>Accepted terms? </th>
+    </tr>${tbleEntries}
+</table>`;
+  let details = document.getElementById("user-entries");
+  details.innerHTML = table;
+};
+
+const saveUserForm = (event) => {
+  event.preventDefault();
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const dob = document.getElementById("dob").value;
+  const acceptTerms = document.getElementById("acceptTerms").checked;
+  var currentYear = new Date().getFullYear();
+  var birthYear = dob.split("-");
+  let year = birthYear[0];
+  var age = currentYear - year;
+  console.log({ age, currentYear, birthYear });
+  if (age < 18 || age > 55) {
+    document.getElementById("dob").style = "border:1px solid red";
+    return alert("Your is not under 18 and 55 years");
+  } else {
+    document.getElementById("dob").style = "border:none";
+
+    const entry = {
+      name,
+      email,
+      password,
+      dob,
+      acceptTerms,
+    };
+    userEntries = retieveEntries();
+    userEntries.push(entry);
+    localStorage.setItem("userEntries", JSON.stringify(userEntries));
+    displayEntries();
+    userForm.reset();
+  }
+};
+userForm.addEventListener("submit", saveUserForm);
+displayEntries();
